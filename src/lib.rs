bc2ed9c2fb859a1a30059e4b4a728ee4f3a8e4f6
Stephen Sherratt <stephen@sherra.tt>
2021-07-06 20:17:56 +1000

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+#[cfg(feature = "serialize")]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
+pub struct Rgba32 {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8,
+}
+
+impl Rgba32 {
+    pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
+    }
+
+    pub const fn new_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b, a: 255 }
+    }
+
+    pub fn to_f32_array_01(self) -> [f32; 4] {
+        [
+            self.r as f32 / 255.,
+            self.g as f32 / 255.,
+            self.b as f32 / 255.,
+            self.a as f32 / 255.,
+        ]
+    }
+}
