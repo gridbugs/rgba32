5fb6937d7c0f88299bf5efcabfb2114ac7bfe6af
Stephen Sherratt <stephen@sherra.tt>
2021-07-10 02:42:49 +1000

    More convenience wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,15 @@ impl Rgba32 {
         Self { r, g, b, a: 255 }
     }
 
+    pub const fn new_grey(x: u8) -> Self {
+        Self {
+            r: x,
+            g: x,
+            b: x,
+            a: 255,
+        }
+    }
+
     pub fn to_f32_array_01(self) -> [f32; 4] {
         [
             self.r as f32 / 255.,
@@ -31,4 +40,12 @@ impl Rgba32 {
 
 pub const fn rgba32(r: u8, g: u8, b: u8, a: u8) -> Rgba32 {
     Rgba32::new(r, g, b, a)
+}
+
+pub const fn rgba32_rgb(r: u8, g: u8, b: u8) -> Rgba32 {
+    Rgba32::new_rgb(r, g, b)
+}
+
+pub const fn rgba32_grey(x: u8) -> Rgba32 {
+    Rgba32::new_grey(x)
 }
